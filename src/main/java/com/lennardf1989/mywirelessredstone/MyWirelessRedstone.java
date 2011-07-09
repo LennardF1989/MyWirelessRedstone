63839aef8023a7a6a3c184478a6d6c8595d9a4d7
Lennard Fonteijn <lennardfonteijn@hotmail.com>
2011-07-09 17:50:52 +0200

    Reimplemented MyDatabase and added my Maven repository to pom.xml

--- src/main/java/com/lennardf1989/mywirelessredstone/MyWirelessRedstone.java
+++ src/main/java/com/lennardf1989/mywirelessredstone/MyWirelessRedstone.java
@@ -1,5 +1,8 @@
 package com.lennardf1989.mywirelessredstone;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.config.Configuration;
 
@@ -7,6 +10,9 @@ import com.avaje.ebean.EbeanServer;
 import com.lennardf1989.bukkitex.MyDatabase;
 import com.lennardf1989.mywirelessredstone.listeners.MWRBlockListener;
 import com.lennardf1989.mywirelessredstone.listeners.MWREntityListener;
+import com.lennardf1989.mywirelessredstone.models.MWRChannel;
+import com.lennardf1989.mywirelessredstone.models.MWRReceiver;
+import com.lennardf1989.mywirelessredstone.models.MWRTransmitter;
 
 public class MyWirelessRedstone extends JavaPlugin {
     private static MyWirelessRedstone plugin;
@@ -58,11 +64,6 @@ public class MyWirelessRedstone extends JavaPlugin {
     private void loadConfiguration() {
         Configuration config = getConfiguration();
         
-        config.setProperty("database.driver", config.getString("database.driver", "org.sqlite.JDBC"));
-        config.setProperty("database.url", config.getString("database.url", "jdbc:sqlite:{DIR}{NAME}.db"));
-        config.setProperty("database.username", config.getString("database.username", "root"));
-        config.setProperty("database.password", config.getString("database.password", ""));
-        config.setProperty("database.isolation", config.getString("database.isolation", "SERIALIZABLE"));
         config.setProperty("database.logging", config.getBoolean("database.logging", false));
         config.setProperty("database.rebuild", config.getBoolean("database.rebuild", true));
         
@@ -72,13 +73,23 @@ public class MyWirelessRedstone extends JavaPlugin {
     private void initializeDatabase() {
         Configuration config = getConfiguration();
         
-        database = new MWRDatabase(this);
+        database = new MyDatabase(this) {
+            protected java.util.List<Class<?>> getDatabaseClasses() {
+                List<Class<?>> list = new ArrayList<Class<?>>();
+                list.add(MWRTransmitter.class);
+                list.add(MWRReceiver.class);
+                list.add(MWRChannel.class);
+        
+                return list;
+            };
+        };
+            
         database.initializeDatabase(
-                config.getString("database.driver"),
-                config.getString("database.url"),
-                config.getString("database.username"),
-                config.getString("database.password"),
-                config.getString("database.isolation"),
+                "org.sqlite.JDBC",
+                "jdbc:sqlite:{DIR}{NAME}.db",
+                "root",
+                "",
+                "SERIALIZABLE",
                 config.getBoolean("database.logging", false),
                 config.getBoolean("database.rebuild", true)
                 );
@@ -91,49 +102,4 @@ public class MyWirelessRedstone extends JavaPlugin {
     public EbeanServer getDatabase() {
         return database.getDatabase();
     }
-    
-    /*
-     * Get all channels from database
-     * New sign -> persist and add to local array
-     */
-    
-    /*database = new MyDatabase(this) {
-        protected java.util.List<Class<?>> getDatabaseClasses() {
-            List<Class<?>> list = new ArrayList<Class<?>>();
-            list.add(MWRSign.class);
-            list.add(MWRTransmitter.class);
-            list.add(MWRReceiver.class);
-            list.add(MWRChannel.class);
-    
-            return list;
-        };
-    };
-    
-    database.initializeDatabase(
-            config.getString("database.driver"),
-            config.getString("database.url"),
-            config.getString("database.username"),
-            config.getString("database.password"),
-            config.getString("database.isolation"),
-            config.getBoolean("database.logging", false),
-            config.getBoolean("database.rebuild", true)
-        );*/
-    
-    /*private void initializeDatabaseHardcoded() {
-        Configuration config = getConfiguration();
-        
-        mwrDatabase = new MWRDatabase(this, getClassLoader());
-        mwrDatabase.initializeDatabase(
-                "org.sqlite.JDBC",
-                "jdbc:sqlite:{DIR}{NAME}.db",
-                "root",
-                "",
-                "SERIALIZABLE",
-                config.getBoolean("database.logging", false),
-                config.getBoolean("database.rebuild", true)
-                );
-        
-        config.setProperty("database.rebuild", false);
-        config.save();
-    }*/
 }
